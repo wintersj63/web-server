39cbc544dd7b47f8655cd545b21e379d4abe6516
Joshua Winters <wintersj63@gmail.com>
2020-08-28 16:40:09 -0500

    updating content to utilize dynamic pages with Handlebars(hbs)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
+
 const path = require('path')
 const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'hbs')
+
 // Using a static page for the site
 app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Author Unknown'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Author Unknown'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Author Unknown', 
+        helpText: 'This is the help page!'
+    })
+})
+
 
 app.get('/weather', (req, res)=> {
     res.send({
