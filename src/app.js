13e2eafb21e187f7e7b07636266870953c23df14
Joshua Winters <wintersj63@gmail.com>
2020-08-28 19:50:47 -0500

    updating the app with partials use

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for express config
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup a static directory to serve
 app.use(express.static(path.join(__dirname, '../public')))
