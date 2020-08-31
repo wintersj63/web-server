426cf6dbabe3b332e13c2400441b47b45860e840
Joshua Winters <wintersj63@gmail.com>
2020-08-31 14:53:07 -0500

    Committing heroku changes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -97,6 +98,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server up on port 3000')
 })
